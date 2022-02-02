4e63267eba2e6769b3034cec34c962fd1acf778e
Chakkapong <chakkapong1999@gmail.com>
2022-02-02 14:41:09 +0700

    fix error

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counter";
 import userReducer from "./lineProfile";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer
   },
 });
